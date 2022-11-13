14cdff98cfde74a0961d3d8f18b2cd7321da125e
sokolov.alex5 <sokolov.alex5@yandex.ru>
2022-11-14 00:54:08 +0300

    Add balances table

--- repository/migrations/20221108145451_init.sql
+++ repository/migrations/20221108145451_init.sql
@@ -1,8 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE users(id BIGINT PRIMARY KEY);
+CREATE TABLE balances(
+    userId BIGINT PRIMARY KEY REFERENCES users (id),
+    value BIGINT
+);
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
+DROP TABLE balances;
 DROP TABLE users;
 -- +goose StatementEnd
