67a55d942692a3b2f72891861143c2745bbc77c9
sokolov.alex5 <sokolov.alex5@yandex.ru>
2022-11-14 00:56:49 +0300

    Add reserve and expanse tables

--- repository/migrations/20221108145451_init.sql
+++ repository/migrations/20221108145451_init.sql
@@ -5,9 +5,30 @@ CREATE TABLE balances(
     userId BIGINT PRIMARY KEY REFERENCES users (id),
     value BIGINT
 );
+CREATE TABLE reserves(
+    id BIGSERIAL PRIMARY KEY,
+    userId BIGINT REFERENCES users (id),
+    size BIGINT
+);
+CREATE INDEX user_reserves ON reserves USING hash (userId);
+CREATE TABLE expanses(
+    id BIGSERIAL PRIMARY KEY,
+    fromId BIGINT REFERENCES users (id),
+    toId BIGINT REFERENCES users (id),
+    ts TIMESTAMP,
+    serviceId BIGINT,
+    orderId BIGINT,
+    cost BIGINT,
+    status SMALLINT
+);
+CREATE INDEX user_expanses ON expanses USING hash (fromId);
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
+DROP INDEX user_expanses;
+DROP TABLE expanses;
+DROP INDEX user_reserves;
+DROP TABLE reserves;
 DROP TABLE balances;
 DROP TABLE users;
 -- +goose StatementEnd
